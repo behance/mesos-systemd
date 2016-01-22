728d0673fd9cc79434ac27bdbc9c6018194e4d8d
anshu pande <anshu@adobe.com>
2016-01-22 10:48:12 -0500

    updated .twistlock in while loop

--- v2/setup/twistlock.sh
+++ v2/setup/twistlock.sh
@@ -11,7 +11,7 @@ sudo docker run --rm \
 
 while read line; do
    etcdctl set $line
-done < ${HOMEDIR}/.marathon
+done < ${HOMEDIR}/.twistlock
 
 
 curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"$twistlockusername", "password":"$twistlockpassword"}' https://adobe.console.twistlock.com:443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://adobe.console.twistlock.com/api/v1/scripts/defender.sh -o defender.sh && chmod a+x defender.sh && sudo ./defender.sh
