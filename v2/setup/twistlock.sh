90958e400c49699bac7e1538dee1d78f72bc17c3
anshu pande <anshu@adobe.com>
2016-01-26 11:29:31 -0500

    removed twistlock.service to get all the logic in twistlock.sh

--- v2/setup/twistlock.sh
+++ v2/setup/twistlock.sh
@@ -13,3 +13,12 @@ while read line; do
   etcdctl set $line
 done < ${HOMEDIR}/.twistlock
 
+
+
+twistlockusername=$(etcdctl get /twistlockusername)
+twistlockpassword=$(etcdctl get /twistlockpassword)
+
+curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"'$(eval echo $twistlockusername)'", "password":"'$(eval echo $twistlockpassword)'"}' https://adobe.console.twistlock.com:443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://adobe.console.twistlock.com/api/v1/cert/client-certs.sh | sh
+
+
+curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"'$(eval echo $twistlockusername)'", "password":"'$(eval echo $twistlockpassword)'"}' https://adobe.console.twistlock.com:443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://adobe.console.twistlock.com/api/v1/scripts/defender.sh -o defender.sh && chmod a+x defender.sh && sudo ./defender.sh
