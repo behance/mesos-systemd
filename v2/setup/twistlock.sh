f4a97f07245d50b685a2f0443af4d39c295a07a6
anshu pande <anshu@adobe.com>
2016-01-27 10:58:40 -0500

    URL parameterized

--- v2/setup/twistlock.sh
+++ v2/setup/twistlock.sh
@@ -8,17 +8,25 @@ sudo docker run --rm \
    -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
     us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .twistlock
 
-
 while read line; do
   etcdctl set $line
 done < ${HOMEDIR}/.twistlock
 
+sudo docker run --rm \
+   -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+    us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .twistlockparameter
+
+
+while read line; do
+  etcdctl set $line
+done < ${HOMEDIR}/.twistlockparameter
 
 
 twistlockusername=$(etcdctl get /twistlockusername)
 twistlockpassword=$(etcdctl get /twistlockpassword)
+twistlockparameter=$(etcdctl get /twistlockparameter)
 
-curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"'$(eval echo $twistlockusername)'", "password":"'$(eval echo $twistlockpassword)'"}' https://adobe.console.twistlock.com:443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://adobe.console.twistlock.com/api/v1/cert/client-certs.sh | sh
+curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"anshu", "password":"anshu"}' https://'$(eval echo $twistlockparameter)':443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://'$(eval echo $twistlockparameter)'/api/v1/cert/client-certs.sh | sh
 
 
-curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"'$(eval echo $twistlockusername)'", "password":"'$(eval echo $twistlockpassword)'"}' https://adobe.console.twistlock.com:443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://adobe.console.twistlock.com/api/v1/scripts/defender.sh -o defender.sh && chmod a+x defender.sh && sudo ./defender.sh
+curl -sSL -k --header "authorization:Bearer $(eval echo $(echo $(curl -s -H "Content-Type: application/json" -d '{"username":"'$(eval echo $twistlockusername)'", "password":"'$(eval echo $twistlockpassword)'"}' https://'$(eval echo $twistlockparameter)':443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" https://'$(eval echo $twistlockparameter)'/api/v1/scripts/defender.sh -o defender.sh && chmod a+x defender.sh && sudo ./defender.sh
