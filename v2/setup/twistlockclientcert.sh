de10aa0f6cc0a282026b0b2e34f53d579388b9f0
anshu pande <anshu@adobe.com>
2016-01-29 11:23:46 -0500

    updated logic to run twistlock as proxy server

--- v2/setup/twistlockclientcert.sh
+++ v2/setup/twistlockclientcert.sh
@@ -18,8 +18,24 @@ twistlockclientusername=$(etcdctl get /twistlockclientusername)
 twistlockclientpassword=$(etcdctl get /twistlockclientpassword)
 twistlockparameter=$(etcdctl get /twistlockparameter)
 
+#steps to generate private cert for each ssh user in HOMEDIR/.docker
+
 curl -sSL -k --header "authorization:Bearer \
 $(eval echo $(echo $(curl -s -H "Content-Type: application/json" \
 -d '{"username":"'$(eval echo $twistlockclientusername)'", "password":"'$(eval echo $twistlockclientpassword)'"}' \
 https://"$(eval echo $twistlockparameter)":443/api/v1/authenticate) | sed -ne 's/.*"token":"\([^,]*\)".*/\1/p'))" \
 https://"$(eval echo $twistlockparameter)"/api/v1/cert/client-certs.sh | sh
+
+#steps to run twistlock as proxy server
+
+etcdctl set DOCKER_HOST tcp://$(eval echo $COREOS_PRIVATE_IPV4):9998
+etcdctl set DOCKER_TLS_VERIFY 1
+
+
+DOCKER_HOST=$(etcdctl get DOCKER_HOST)
+export DOCKER_HOST
+
+DOCKER_TLS_VERIFY=$(etcdctl get DOCKER_TLS_VERIFY)
+export DOCKER_TLS_VERIFY
+
+
