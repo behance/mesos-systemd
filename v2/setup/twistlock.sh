11ce86eeb7e6aeb78d3038ae707630f91034b650
anshu pande <anshu@adobe.com>
2016-02-10 10:06:59 -0500

    added just the defender installation

---
+++ v2/setup/twistlock.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+source /etc/environment
+
+HOMEDIR=$(eval echo "~`whoami`")
+
+sudo docker run --rm \
+   -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+    us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .twistlock
+
+while read line; do
+  etcdctl set $line
+done < ${HOMEDIR}/.twistlock
+
+sudo docker run --rm \
+   -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
+    us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .twistlockparameter
+
+
+while read line; do
+  etcdctl set $line
+done < ${HOMEDIR}/.twistlockparameter
