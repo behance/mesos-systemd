8345bf27b24a5c0402005c8c262617bcd9475714
anshu pande <anshu@adobe.com>
2016-02-10 15:09:17 -0500

    still testing

--- v2/setup/twistlock-user.sh
+++ v2/setup/twistlock-user.sh
@@ -6,12 +6,12 @@ HOMEDIR=$(eval echo "~`whoami`")
 
 sudo docker run --rm \
     -v ${HOMEDIR}:/data/ behance/docker-aws-s3-downloader \
-     us-east-1 $CONTROL_TIER_S3SECURE_BUCKET ."$(echo $USER)"
+     us-east-1 $CONTROL_TIER_S3SECURE_BUCKET .core
 
 
 while read line; do
   etcdctl set $line
-done < ${HOMEDIR}/."$(echo $USER)"
+done < ${HOMEDIR}/.core
 
 
 twistlockclientusername=$(etcdctl get /twistlockclientusername)
