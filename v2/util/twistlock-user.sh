91c0d89851f8c4ba4fca19b92350640b900b56f5
Anshu Pande <anshu.pande@gmail.com>
2016-02-24 17:13:51 -0500

    removed proxy lines from twistlock-user.sh

--- v2/util/twistlock-user.sh
+++ v2/util/twistlock-user.sh
@@ -42,8 +42,8 @@ for i in `ls /home`;
 done
 #steps to run twistlock as proxy server
 
-echo "export DOCKER_HOST=tcp://$(eval echo $COREOS_PRIVATE_IPV4):9998" >> /etc/environment
-echo "export DOCKER_TLS_VERIFY=1" >> /etc/environment
+#echo "export DOCKER_HOST=tcp://$(eval echo $COREOS_PRIVATE_IPV4):9998" >> /etc/environment
+#echo "export DOCKER_TLS_VERIFY=1" >> /etc/environment
 
 
 
