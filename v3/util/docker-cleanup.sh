5d5b434ab6aa7f06bb0134b6fbe8d0bc497476c4
Chris Fortescue <fortescu@adobe.com>
2016-05-13 16:28:17 +0000

    Remove the ability to remove any layer for now

--- v3/util/docker-cleanup.sh
+++ v3/util/docker-cleanup.sh
@@ -3,14 +3,16 @@
 # NOTE: this script will need to be sudo'ed for access to /var/lib/docker
 
 echo "Removing dead containers & volumes"
-docker rm -v $(docker ps -a|grep Exited|cut -d" " -f1) 2> /dev/null
+docker rm -v $(docker ps -a|grep Exited|cut -d" " -f1)  #2> /dev/null
 
-echo "Removing images"
-docker rmi $(docker images -aq) 2> /dev/null
+#echo "Removing images"
+#docker rmi $(docker images -aq) 2> /dev/null
 
 # play on https://github.com/docker/docker/issues/6354#issuecomment-114688663
 FSDRIVER=$(docker info|grep Storage|cut -d: -f2|tr -d [:space:])
-
+echo "Driver $FSDRIVER"
 echo "---- Complete ----"
+
 sudo free -h
-sudo df -Th
+sudo lvdisplay
+
