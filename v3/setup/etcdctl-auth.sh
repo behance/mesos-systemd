b5d09401d6a5b349b7a79d1a14184074913a613c
anshu pande <anshu@adobe.com>
2016-05-18 12:18:52 -0400

    remove comments not required

--- v3/setup/etcdctl-auth.sh
+++ v3/setup/etcdctl-auth.sh
@@ -35,15 +35,12 @@ add_users $READ_USERNAME $READ_PASSWORD
 add_users $WRITE_USERNAME $WRITE_PASSWORD
 
 # add root-user
-#sudo echo '{"user":"'${ROOT_USERNAME}'", "password":"'${ROOT_PASSWORD}'"}' > $CRED_DIR/root-user.json
 curl -L http://127.0.0.1:2379/v2/auth/users/${ROOT_USERNAME} -XPUT -d "@$CRED_DIR/root.json"
 
 # add read-user
-#sudo echo '{"user":"'${READ_USERNAME}'", "password":"'${READ_PASSWORD}'"}' > $CRED_DIR/read-user.json
 curl -L http://127.0.0.1:2379/v2/auth/users/${READ_USERNAME} -XPUT -d "@$CRED_DIR/read-user.json"
 
 # add write-user
-#sudo echo '{"user":"'${WRITE_USERNAME}'", "password":"'${WRITE_PASSWORD}'"}' > $CRED_DIR/write-user.json
 curl -L http://127.0.0.1:2379/v2/auth/users/${WRITE_USERNAME} -XPUT -d "@$CRED_DIR/write-user.json"
 
 # read access
@@ -63,7 +60,6 @@ sudo echo '{"user": "'${WRITE_USERNAME}'", "grant": ["read-write"]}' > $CRED_DIR
 curl -L http://127.0.0.1:2379/v2/auth/users/${WRITE_USERNAME} -XPUT -d "@$CRED_DIR/read-write.json"
 
 # Enable authentication
-#curl -L http://127.0.0.1:2379/v2/auth/enable -XPUT
 etcdctl auth enable
 
 sudo rm -rf $CRED_DIR
