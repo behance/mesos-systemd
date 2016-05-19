82b4d2f0285a024f5cde4fbcc4894f85b3b2e41b
Chris Fortescue <fortescu@adobe.com>
2016-05-19 21:50:02 +0000

    add a comment.  cleanup

--- v3/util/docker-cleanup.sh
+++ v3/util/docker-cleanup.sh
@@ -3,11 +3,11 @@
 # NOTE: this script will need to be sudo'ed for access to /var/lib/docker
 
 echo "Removing dead containers & volumes"
-docker rm -v $(docker ps -a|grep Exited|cut -d" " -f1)  #2> /dev/null
+docker rm -v $(docker ps -a|grep Exited|cut -d" " -f1)  2> /dev/null
 
+# top-level orphaned images have <none> in name.  rmi these
+# rmi them one at a time so it's clear what is being done in the logs
 docker images | grep none | awk '{print $3}' | xargs -n 1 -IXX docker rmi XX
-#echo "Removing images"
-#docker rmi $(docker images -aq) 2> /dev/null
 
 # play on https://github.com/docker/docker/issues/6354#issuecomment-114688663
 FSDRIVER=$(docker info|grep Storage|cut -d: -f2|tr -d [:space:])
